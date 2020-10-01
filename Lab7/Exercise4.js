33df1c64c1b2420343d785a3769fa424dbe0c6fb
csebastiana <70238006+csebastiana@users.noreply.github.com>
2020-10-01 10:37:07 -1000

    Add files via upload

---
+++ Lab7/Exercise4.js
@@ -0,0 +1,16 @@
+// -- Price depends on quantity
+quantity = 100;
+
+if (quantity >= 25) {
+    price = 35;
+}
+else if (quantity >= 10) {
+    price = 50;
+}
+else if (quantity > 0) {
+    price = 100;
+}
+else
+    price = "no purchase";
+
+console.log(quantity + ' products will cost ' + price + ' each.');
